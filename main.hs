5531feda3187a5188b9dfec4dc44b27e731c3558
Bruno Fernandes (up202108871) <up202108871@up.pt>
2023-12-28 17:20:50 +0000

    All tests pass in assembler, except the test with the loop

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Ord (comparing)
 
 data Inst =
   Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | Store String | Noop |
@@ -71,6 +72,18 @@ fetch var stack store = (value:stack, store)
                   Just value -> value
                   Nothing -> error "Run-time error"
 
+-- Pops a value from the stack and stores it in the variable with the given name
+-- If the variable already exists, it will be overwritten
+store :: String -> Stack -> State -> (Stack, State)
+store var (value:stack) state = (stack, updateState var value state)
+
+-- Updates the value of the variable if it exists, if not adds it to the state
+updateState :: String -> StackValue -> State -> State
+updateState var value [] = [(var, value)]
+updateState var value ((existingVar, existingValue):rest)
+  | (var == existingVar) = (var, value) : rest
+  | otherwise = (existingVar, existingValue) : updateState var value rest
+
 -- Will change the flow of control:  if the top of the stack is the
 --value tt (that is, some boolean expression has been evaluated to true), then the
 --stack is popped and c1 is to be executed next. Otherwise, if the top element of the
@@ -84,15 +97,13 @@ branch _ _ _ _ = error "Run-time error"
 noop :: Stack -> State -> (Stack, State)
 noop stack store = (stack, store)
 
-
-
 -- Creates an empty stack
 createEmptyStack :: Stack
 createEmptyStack = []
 
 -- Converts a stack to a string representation
 stack2Str :: Stack -> String
-stack2Str stack = intercalate "," (map stackValueToStr (reverse stack))
+stack2Str stack = intercalate "," (map stackValueToStr stack)
 
 -- Converts a stack value to a string representation
 stackValueToStr :: StackValue -> String
@@ -103,41 +114,40 @@ stackValueToStr (BoolValue b) = show b
 createEmptyState :: State
 createEmptyState = []
 
--- Converts a state to a string representation
+-- Converts a state to a string representation and sorts it
 state2Str :: State -> String
-state2Str = intercalate "," . map (\(var, value) -> var ++ "=" ++ stackValueToStr value)
-
+state2Str = intercalate "," . map (\(var, value) -> var ++ "=" ++ stackValueToStr value) . sortByName
+  where
+    sortByName = sortBy (comparing fst)
 
 exec ::Inst -> Stack -> State -> (Stack, State)
-exec (Push n) stack store = (push n stack, store)
-exec Add stack store = (add stack, store)
-exec Mult stack store = (mult stack, store)
-exec Sub stack store = (sub stack, store)
-exec Tru stack store = (tru stack, store)
-exec Fals stack store = (fals stack, store)
-exec Equ stack store = (equ stack, store)
-exec Le stack store = (le stack, store)
-exec And stack store = (and' stack, store)
-exec Neg stack store = (neg stack, store)
-exec (Fetch var) stack store = fetch var stack store
-exec (Store var) (value:stack) store = (stack, (var, value):store)
-exec Noop stack store = noop stack store
---exec (Branch c1 c2) stack store = (c, stack, store)
-  --where (c, _, _) = branch (c1 c2) stack store
---exec (Loop c1 c2) stack store = (c, stack, store)
- -- where (c, _, _) = branch (c1 ++ [Loop c1 c2]) [Noop] stack store
+exec (Push n) stack state = (push n stack, state)
+exec Add stack state = (add stack, state)
+exec Mult stack state = (mult stack, state)
+exec Sub stack state = (sub stack, state)
+exec Tru stack state = (tru stack, state)
+exec Fals stack state = (fals stack, state)
+exec Equ stack state = (equ stack, state)
+exec Le stack state = (le stack, state)
+exec And stack state = (and' stack, state)
+exec Neg stack state = (neg stack, state)
+exec (Fetch var) stack state = fetch var stack state
+exec (Store var) stack state = store var stack state
+exec Noop stack state = noop stack state
+-- exec (Branch c1 c2) stack state = (c, stack, state)
+--  where (c, _, _) = branch c1 c2
+--exec (Loop c1 c2) stack state = (c, stack, state)
+--  where (c, _, _) = branch (c1 ++ [Loop c1 c2]) [Noop]
 
 -- Runs the given code with the given stack and state and returns the resulting stack and state
---given a list of instructions (type defined as Code, i.e. type Code = [Inst]), a stack (type defined as
---Stack) and that is initially empty, and a storage (type defined as State), runs the
---list of instructions returning as ouput an empty code list, a stack and the output
---values in the storage.
+-- given a list of instructions (type defined as Code, i.e. type Code = [Inst]), a stack (type defined as
+-- Stack) and that is initially empty, and a storage (type defined as State), runs the
+-- list of instructions returning as ouput an empty code list, a stack and the output
+-- values in the storage.
 run :: (Code, Stack, State) -> (Code, Stack, State)
 run ( [], stack , store ) = ( [] , stack , store )
 run (code , stack , state) = run ( tail code , stack' , state' )
   where (stack', state') = exec ( head code ) stack state
-
-
 
 -- To help you test your assembler
 testAssembler :: Code -> (String, String)
@@ -145,25 +155,30 @@ testAssembler code = (stack2Str stack, state2Str state)
   where (_,stack,state) = run (code, createEmptyStack, createEmptyState)
 
 -- Examples:
--- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
--- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
--- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
--- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
--- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
--- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
--- testAssembler [Push (-20),Push (-21), Le] == ("True","")
--- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+-- DONE -- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+-- DONE -- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
+-- DONE -- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
+-- DONE -- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
+-- DONE -- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
+-- DONE -- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
+-- DONE -- testAssembler [Push (-20),Push (-21), Le] == ("True","")
+-- DONE -- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
 -- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
 -- If you test:
--- testAssembler [Push 1,Push 2,And]
+-- DONE -- testAssembler [Push 1,Push 2,And]
 -- You should get an exception with the string: "Run-time error"
 -- If you test:
--- testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
+-- DONE -- testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
 -- You should get an exception with the string: "Run-time error"
 
 -- Part 2
 
 -- TODO: Define the types Aexp, Bexp, Stm and Program
+
+-- type Aexp = undefined
+-- type Bexp = undefined
+-- type Stm = undefined
+-- type Program = undefined
 
 -- compA :: Aexp -> Code
 compA = undefined -- TODO
